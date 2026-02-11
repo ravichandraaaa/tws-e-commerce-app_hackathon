73363369cb6147d1defa5c1e07ae2209a7ab21b8
ravichandraaaa <ravichandrakotapati68@gmail.com>
2026-02-12 01:03:13 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-s3-backend-tws-hackat"
     key            = "backend-locking"
-    region         = "eu-west-1"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
